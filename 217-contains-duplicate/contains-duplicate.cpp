4395a0163afa97201aedf25e490767430d66f4db
AtharvaDevp1 <av.devp.prog@gmail.com>
2026-09-18 20:03:25 +0530

    Time: 94 ms (13.49%) | Memory: 113.3 MB (23.18%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> map;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            map[nums[i]]+=1;
+        }
+
+        for(int i=0;i<n;i++){
+           if(map[nums[i]] > 1){
+            return true;
+           }
+        }
+        return false;
+    }
+};
